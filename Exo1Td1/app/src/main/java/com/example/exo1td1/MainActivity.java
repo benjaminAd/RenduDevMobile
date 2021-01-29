b1293d8dee32a0f156a8cc8d18bbe9652253b075
Benjamin <adolphe906@gmail.com>
2021-01-29 15:02:44 +0100

    Exo 1 et 1.2 TD1

---
+++ Exo1Td1/app/src/main/java/com/example/exo1td1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.exo1td1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
+    }
+}
