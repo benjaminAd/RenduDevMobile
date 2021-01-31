0428033922384224b9f2011317d12b49c45bc034
Benjamin <adolphe906@gmail.com>
2021-01-31 09:46:07 +0100

    Modification du code des exos 1 et 1.2

--- Exo1Td1/app/src/main/java/com/example/exo1td1/MainActivity.java
+++ Exo1Td1/app/src/main/java/com/example/exo1td1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.exo1td1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +12,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);*/
+
+        //Modification de code
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
         TextView tv = new TextView(this);
         tv.setText("Hello, Android");
-        setContentView(tv);
+        layout.addView(tv);
+        EditText editText = new EditText(this);
+        editText.setHint("Entre du texte");
+        layout.addView(editText);
+        setContentView(layout);
+
     }
 }
